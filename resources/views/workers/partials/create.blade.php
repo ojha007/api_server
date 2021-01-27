a2697e9ebb5ad6cf892dbea99365c5174e7f63a3
Nutan_1 <dahalnutan66@gmail.com>
2021-01-27 23:03:55 +0545

    workers create form

--- resources/views/workers/partials/create.blade.php
+++ resources/views/workers/partials/create.blade.php
@@ -9,17 +9,7 @@
 @section('breadcrumb') @endsection
 @section('content')
     <div class="box box-default border-0">
-        <div class="box-header">
-            <h3 class="box-title"></h3>
-            <div class="box-tools pull-right">
-                <a href="{{route('workers.create')}}">
-                    <i class="fa fa-plus"></i>
-                    Create Worker
-                </a>
-            </div>
-        </div>
         <div class="box-body">
-
 @include('workers.partials.form')
         </div>
     </div>
