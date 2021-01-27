e6f117a3366af56970ad6782ab600af8681b80dd
ojha007 <ojhaamir007@gmail.com>
2021-01-27 22:26:54 +0545

    worker fixes

---
+++ resources/views/workers/partials/form.blade.php
@@ -0,0 +1,5 @@
+{!! Form::bsText('name',old('name'),['autofocus']) !!}
+{!! Form::bsText('email',old('name'),['autofocus']) !!}
+{!! Form::bsText('phone',old('name'),['autofocus']) !!}
+{{--{!! Form::bsText('name',old('name'),['autofocus']) !!}--}}
+
