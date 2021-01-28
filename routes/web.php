0a8b7172a8c9cfb73d0f3f84e1f28771a8c3a89e
ojha007 <ojhaamir007@gmail.com>
2021-01-28 09:57:57 +0545

    worker fixes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::any('/', function (){
+    return view('welcome');
+});
 Route::group(['middleware' => 'auth'], function ($router) {
-    $router->any('/home', 'DashboardController@dashboard')->name('dashboard');
+    $router->get('/home', 'DashboardController@dashboard')->name('dashboard');
     $router->resource('upcoming-schedule', 'UpcomingScheduleController');
     $router->resource('enquiries', 'EnquiryController');
     $router->resource('quotations', 'QuotationController');
