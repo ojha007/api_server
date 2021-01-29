009d6014c74b8a4bad35c87b66760a712030970d
ojha007 <ojhaamir007@gmail.com>
2021-01-29 13:52:58 +0545

    fixes

--- app/Http/Controllers/Api/WorkerController.php
+++ app/Http/Controllers/Api/WorkerController.php
@@ -7,6 +7,8 @@ namespace App\Http\Controllers\Api;
 class WorkerController extends \App\Http\Controllers\WorkerController
 {
 
+    protected $repository;
+
     public function __construct()
     {
         $this->middleware('auth:api');
