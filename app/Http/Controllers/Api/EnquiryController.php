51aba08d6fd5709972efe08b9385ca7815dcf6aa
ojha007 <ojhaamir007@gmail.com>
2021-01-29 14:23:12 +0545

    fixes

--- app/Http/Controllers/Api/EnquiryController.php
+++ app/Http/Controllers/Api/EnquiryController.php
@@ -4,11 +4,17 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Models\Enquiry;
+use App\Repositories\EnquiryRepository;
+
 class EnquiryController extends \App\Http\Controllers\EnquiryController
 {
+
+    protected $repository;
 
     public function __construct()
     {
         $this->middleware('auth:api');
+        $this->repository = new EnquiryRepository(new Enquiry());
     }
 }
