d7f645abb90befd963fe1d0cb6acf5f4a6e502f7
ojha007 <ojhaamir007@gmail.com>
2021-02-05 17:20:03 +0545

    fixes

--- routes/web.php
+++ routes/web.php
@@ -43,10 +43,10 @@ Route::get('/oauth/gmail', function () {
 
 Route::get('/oauth/gmail/callback', function () {
     LaravelGmail::makeToken();
-    return redirect()->to('/');
+    return redirect()->to('/home');
 });
 
 Route::get('/oauth/gmail/logout', function () {
     LaravelGmail::logout(); //It returns exception if fails
-    return redirect()->to('/');
+    return redirect()->to('/home');
 });
