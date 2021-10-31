b237f307c11151d7c21f9100b074a3216c857f85
Amir Ojha <ojhaamir007@gmail.com>
2021-10-31 21:20:58 +0545

    fixes issues and added queue

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -5,7 +5,6 @@ namespace App\Events;
 use App\Models\ChatMessage;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -36,12 +35,12 @@ class MessageSent implements ShouldBroadcast
     /**
      * Get the channels the event should broadcast on.
      *
-     * @return PrivateChannel
+     * @return Channel
      */
-    public function broadcastOn(): PrivateChannel
+    public function broadcastOn(): Channel
     {
 
-        return new PrivateChannel('mibsoftClientChat.' . $this->identifier);
+        return new Channel('mibsoftClientChat.' . $this->identifier);
     }
 
     public function broadcastAs(): string
