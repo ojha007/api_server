c308ccc6aaee3812be80af9d656bf6dab2248290
Amir Ojha <ojhaamir007@gmail.com>
2022-02-28 21:51:51 +0545

    task status

--- resources/views/enquiry/index.blade.php
+++ resources/views/enquiry/index.blade.php
@@ -17,10 +17,12 @@
                 <thead>
                 <tr>
                     <td>Name</td>
-                    <td>Contact</td>
-                    <td>Address</td>
-                    <td>Pickup Date/Time</td>
-                    <td>Quotation</td>
+                    <td>Email</td>
+                    {{--                    <td>Address</td>--}}
+                    {{--                    <td>Pickup Date/Time</td>--}}
+                    <td>Phone</td>
+                    <td>Title</td>
+                    <td>Quotation Send</td>
                     <td>Actions</td>
                 </tr>
                 </thead>
@@ -28,23 +30,26 @@
                 @forelse($enquiries as $enquiry)
                     <tr>
                         <td>{{$enquiry->name}}</td>
-                        <td>{{$enquiry->email}} -- {{$enquiry->mobile_number}}</td>
-                        <td>{{$enquiry->address1}} -- {{$enquiry->city}}</td>
-                        <td>{{$enquiry->pickup_date}}</td>
-                        <td>{{$enquiry->quotation->title ?? ''}}</td>
+                        <td>{{$enquiry->email}}</td>
+                        <td>{{$enquiry->phone}}</td>
+                        <td>{{$enquiry->title}}</td>
                         <td>
-                            <a class="btn btn-success btn-flat btn-xs"
+                            @if($enquiry->quotation_id)
+                                <a href="{{route('quotations.show',$enquiry->quotation_id)}}">
+                                    {{$enquiry->quotation->title ?? ''}}
+                                </a>
+                            @else
+                                NO QUOTATION SEND
+                            @endif
+                        </td>
+                        <td>
+                            <a class="btn btn-default btn-flat "
                                href="{{route('enquiries.show',$enquiry->id)}}"
                                title="Show Enquiry">
                                 <i class="fa fa-eye"></i>
 
                             </a>
-{{--                            <a class="btn btn-default btn-flat btn-sm"--}}
-{{--                               href="{{route('enquiries.show-quotations',$enquiry->id)}}"--}}
-{{--                               title="Send Quotations">--}}
-{{--                                <i class="fa fa-mail-reply"></i>--}}
 
-{{--                            </a>--}}
                         </td>
                     </tr>
                 @empty
