47fdc0fff1377c12e907a598f1cdcf5e6077c583
Amir Ojha <ojhaamir007@gmail.com>
2022-02-28 20:11:49 +0545

    task status

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -4,29 +4,55 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Responses\SuccessResponse;
+use App\Models\TaskStatus;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
 
     private function totalWorker(): int
     {
-        return 10;
+        return DB::table('users')
+            ->join('model_has_roles', 'model_id', '=', 'users.id')
+            ->where('model_type', '=', 'App\\Models\\User')
+            ->count('users.id');
+
     }
 
     private function totalTask(): int
     {
-        return 100;
+
+        return DB::table('users')
+            ->join('task_workers as task', 'users.id', '=', 'task.worker_id')
+            ->count('task.id');
     }
 
     private function completedTask(): int
     {
-        return 10;
+        $sub = DB::table('task_status')
+            ->select('task_id')
+            ->where('status', '=', TaskStatus::COMPLETED)
+            ->groupBy('task_id');
+        return DB::table('users')
+            ->join('task_status as taskStatus', 'users.id', '=', 'taskStatus.user_id')
+            ->joinSub($sub, 'completedTask', 'completedTask.task_id', '=', 'taskStatus.id')
+            ->where('users.id', '=', auth()->id())
+            ->count('taskStatus.id');
 
     }
 
     private function rejectedTask(): int
     {
-        return 1;
+        $sub = DB::table('task_status')
+            ->select('task_id')
+            ->where('status', '=', TaskStatus::REJECTED)
+            ->groupBy('task_id');
+        return DB::table('users')
+            ->join('task_status as taskStatus', 'users.id', '=', 'taskStatus.user_id')
+            ->joinSub($sub, 'rejectedTask', 'rejectedTask.task_id', '=', 'taskStatus.id')
+            ->where('users.id', '=', auth()->id())
+            ->count('taskStatus.id');
+
     }
 
     public function index(): SuccessResponse
