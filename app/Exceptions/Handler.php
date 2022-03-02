7e725bb82b3fc5fc12ef64fe3598606f79f9bdb4
Amir Ojha <ojhaamir007@gmail.com>
2022-03-02 12:19:04 +0545

    time query

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,7 +42,7 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $e)
     {
-        dd($e);
+//        dd($e);
         if ($request->wantsJson()) {
 
             //add Accept: application/json in request
