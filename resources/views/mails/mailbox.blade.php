bdd23aa2cbfdde2e71544f5850fc2c2e9074dc34
Amir Ojha <ojhaamir007@gmail.com>
2022-03-03 22:44:01 +0545

    mail component

--- resources/views/mails/mailbox.blade.php
+++ resources/views/mails/mailbox.blade.php
@@ -22,30 +22,9 @@
             <dashboard-mail url="{{route('mails.inbox')}}"
                             logged="{{\LaravelGmail::check()}}">
             </dashboard-mail>
-            <!-- /.box-header -->
-{{--            <div class="box-body " id="email">--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-xs-12 text-center">--}}
-{{--                        @if(\LaravelGmail::check())--}}
-{{--                            <button type="button" class="btn btn-default btn-lg">--}}
-{{--                                <i class="fa fa-spin fa-refresh"></i>&nbsp; Loading Mails--}}
-{{--                            </button>--}}
 
-{{--                        @else--}}
-{{--                            <a href="{{ url('oauth/gmail') }}" class="nav-link">Login to Your gmail account</a>--}}
-{{--                        @endif--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <!-- /.mail-box-messages -->--}}
-{{--            </div>--}}
-        {{--            <div class="overlay">--}}
-        {{--                <i class="fa fa-refresh fa-spin"></i>--}}
-        {{--            </div>--}}
-        <!-- /.box-body -->
         </div>
         <!-- /. box -->
     </div>
 @endsection
-{{--@push('scripts')--}}
-{{--    @include('mails.scripts',['url'=>$url])--}}
-{{--@endpush--}}
+
